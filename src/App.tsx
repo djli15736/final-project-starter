40292381d7985fb962fba4980c08f30dfa0bdad7
djli15736 <djli15736@gmail.com>
2021-10-07 12:01:42 -0400

    added button that increases and decrease counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { CounterBox } from "./components/CounterBox";
 
 function App(): JSX.Element {
+    const [counter, setCounter] = useState(0);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
+            <CounterBox counter={counter} setCounter={setCounter}></CounterBox>
         </div>
     );
 }
