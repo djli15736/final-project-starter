0284a0437ed55742aff4cb2797d9b82e72e5166e
jwdagney55 <jwdagney@udel.edu>
2021-10-07 19:48:02 -0400

    Working welcome message modal

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import Modal from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 
 export default function Welcome(): JSX.Element{
+  
+    const [visible, setVisible] = React.useState<boolean>(true);
+
+    const handleClick = () => setVisible(false);
+    
 
     return (
-
-        <div className="modal" tabIndex={1} role="dialog">
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Welcome to The Game!</h5>
-                    </div>
-                    <div className="modal-body">
-                        <p>Enjoy playing the Game</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <Modal show={visible}>
+            <Modal.Header closeButton>
+                <Modal.Title>Welcome to the Game!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div>We hope you enjoy it. Click close to exit this prompt.</div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={handleClick}>Close</Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
